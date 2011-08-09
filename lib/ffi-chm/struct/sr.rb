47a3cbe04dad6fb3ad946fb9becc54f10db77dd2
nanki <nanki@dotswitch.net>
2011-08-10 07:34:24 +0900

    WCLs is not byte aligned.
    Add tests for r > 3.

--- lib/ffi-chm/struct/sr.rb
+++ lib/ffi-chm/struct/sr.rb
@@ -5,8 +5,6 @@ class FFI::Chm::Struct::SR < BinData::Primitive
   array :prefix, :type => :bit1, :read_until => lambda { element.zero? }
   array :bits, :type => :bit1, :initial_length => :num_bits
 
-  resume_byte_alignment
-
   def num_bits
     get_parameter(:r) + [0, prefix.size - 2].max
   end
@@ -14,7 +12,7 @@ class FFI::Chm::Struct::SR < BinData::Primitive
   def get
     size = prefix.size
     ret = bits.inject(0){|r, v| r << 1 | v}
-    ret += 2 ** (size + 1) if size > 1
+    ret += 2 ** (get_parameter(:r) + size - 2) if size > 1
     ret
   end
 end
--- test/test_sr.rb
+++ test/test_sr.rb
@@ -4,39 +4,50 @@ require "ffi-chm"
 class TestSR < Test::Unit::TestCase
   def setup
     @testset = [
-      [ 0, '0000----'],
-      [ 1, '0001----'],
-      [ 2, '0010----'],
-      [ 7, '0111----'],
-      [ 8, '10000---'],
-      [ 9, '10001---'],
-      [10, '10010---'],
-      [15, '10111---'],
-      [16, '1100000-'],
-      [17, '1100001-'],
-      [18, '1100010-'],
-      [30, '1101110-'],
-      [31, '1101111-'],
-      [32, '111000000-------'],
-      [33, '111000001-------'],
-      [34, '111000010-------'],
-      [62, '111011110-------'],
-      [63, '111011111-------'],
-      [64, '1111000000------']
-    ].each{|set|set.last.gsub!('-', '0')}
+      [3,   0, '0000----'],
+      [3,   1, '0001----'],
+      [3,   2, '0010----'],
+      [3,   7, '0111----'],
+      [3,   8, '10000---'],
+      [3,   9, '10001---'],
+      [3,  10, '10010---'],
+      [3,  15, '10111---'],
+      [3,  16, '1100000-'],
+      [3,  17, '1100001-'],
+      [3,  18, '1100010-'],
+      [3,  30, '1101110-'],
+      [3,  31, '1101111-'],
+      [3,  32, '111000000-------'],
+      [3,  33, '111000001-------'],
+      [3,  34, '111000010-------'],
+      [3,  62, '111011110-------'],
+      [3,  63, '111011111-------'],
+      [3,  64, '1111000000------'],
+      [5,  31, '011111----------'],
+      [5,  63, '1011111----------'],
+      [5, 512, '111110000000000-']
+    ]
   end
 
   def test_sr
-    @testset.each do |expect, bits|
-      sr = FFI::Chm::Struct::SR.new(:r => 3).read [bits].pack("B*")
+    @testset.each{|set|set.last.gsub!('-', '0')}
+    @testset.each do |r, expect, bits|
+      sr = FFI::Chm::Struct::SR.new(:r => r).read [bits].pack("B*")
       assert_equal expect, sr.get
     end
   end
 
   def test_sr_byte_aligned
-    expects, bits = @testset.transpose
+    @testset.each{|set|set.last.delete!('-')}
+
+    _, expects, bits = @testset.select{|v|v.first == 3}.transpose
     bytes = [bits.join].pack("B*")
     values = BinData::Array.new(:type => [:sr, {:r => 3}], :read_until => :eof).read(bytes)
+    assert_equal expects, values
+
+    _, expects, bits = @testset.select{|v|v.first == 5}.transpose
+    bytes = [bits.join].pack("B*")
+    values = BinData::Array.new(:type => [:sr, {:r => 5}], :read_until => :eof).read(bytes)
     assert_equal expects, values
   end
 end
