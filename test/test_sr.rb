681413c46016c6c3b4c12a037435c650efc6975d
nanki <nanki@dotswitch.net>
2011-08-11 09:42:43 +0900

    Correct test.
    Wrong bitstream for 64(r = 3).

--- test/test_sr.rb
+++ test/test_sr.rb
@@ -4,6 +4,8 @@ require "ffi-chm"
 class TestSR < Test::Unit::TestCase
   def setup
     @testset = [
+      [2,   3, '011----'],
+      [2,   4, '1000----'],
       [3,   0, '0000----'],
       [3,   1, '0001----'],
       [3,   2, '0010----'],
@@ -22,7 +24,7 @@ class TestSR < Test::Unit::TestCase
       [3,  34, '111000010-------'],
       [3,  62, '111011110-------'],
       [3,  63, '111011111-------'],
-      [3,  64, '1111000000------'],
+      [3,  64, '11110000000-----'],
       [5,  31, '011111----------'],
       [5,  63, '1011111----------'],
       [5, 512, '111110000000000-']
@@ -40,14 +42,13 @@ class TestSR < Test::Unit::TestCase
   def test_sr_byte_aligned
     @testset.each{|set|set.last.delete!('-')}
 
-    _, expects, bits = @testset.select{|v|v.first == 3}.transpose
-    bytes = [bits.join].pack("B*")
-    values = BinData::Array.new(:type => [:sr, {:r => 3}], :read_until => :eof).read(bytes)
-    assert_equal expects, values
+    rs, expects, bits = @testset.transpose
+    bits = bits.join
+    bits += "0" * bits.size % 8
+    wcls = BinData::IO.new [bits].pack("B*")
 
-    _, expects, bits = @testset.select{|v|v.first == 5}.transpose
-    bytes = [bits.join].pack("B*")
-    values = BinData::Array.new(:type => [:sr, {:r => 5}], :read_until => :eof).read(bytes)
+    values = rs.map{|r| FFI::Chm::Struct::SR.new(:r => r).read(wcls)}
+
     assert_equal expects, values
   end
 end
