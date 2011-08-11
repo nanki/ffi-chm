714ad94caf9f1239beff5c2c237a91982e1c55a7
nanki <nanki@dotswitch.net>
2011-08-11 10:21:01 +0900

    Use eval_parameter instead of get_parameter.
    That's the way of BinData.

--- lib/ffi-chm/struct/sr.rb
+++ lib/ffi-chm/struct/sr.rb
@@ -6,13 +6,13 @@ class FFI::Chm::Struct::SR < BinData::Primitive
   array :bits, :type => :bit1, :initial_length => :num_bits
 
   def num_bits
-    get_parameter(:r) + [0, prefix.size - 2].max
+    eval_parameter(:r) + [0, prefix.size - 2].max
   end
 
   def get
     size = prefix.size
     ret = bits.inject(0){|r, v| r << 1 | v}
-    ret += 2 ** (get_parameter(:r) + size - 2) if size > 1
+    ret += 2 ** (eval_parameter(:r) + size - 2) if size > 1
     ret
   end
 end
